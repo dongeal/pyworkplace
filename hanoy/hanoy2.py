1dab089eab9b113c52330d4a64b6573bd05934b8
dongeal <dongeal@gmail.com>
2023-07-04 12:26:06 +0900

    심플한 하노이

--- hanoy/hanoy2.py
+++ hanoy/hanoy2.py
@@ -5,42 +5,28 @@ MOVE_MESSAGE = "{}번 원반을 {}에서 {}로 이동"
       
         # print(MOVE_MESSAGE.format(N, start, destination))
         
-        # p1[N-1,start-1]=0
-        # p1[N-1,destination-1]=N
-        # p2=np.sort(p1,axis=0)
-        # print(p2)
         
-        # for h in range(0,tower_number):
-        #     for w in range(0,3):
-        #         img[100+h*10:110+h*10, 100+w*100:200+w*100]= t[p2[h,w]]
-                
-        #         cv2.imshow('hanoi', img)
-       
-        
-        # cv2.waitKey(1500)
 count = 0      
 def hanoi(n, start, destination, via):
-   
+    
     global count
-    # 원반이 1개일 때 시작 기둥에서 도착 기둥까지 한 번에 이동
+    # 원반이 1개일 때 시작 기둥에서 도착 기둥까지 한 번에 실질적인이동
     if n <= 1:
         print(MOVE_MESSAGE.format(n, start, destination))
-        count += 1
+        count += 1   # 실질적인 이동 카운트
         
     else :
         
-    # 원반 n-1개를 시작 기둥에서 보조 기둥으로 이동
+    # 원반 n-1개를 시작 기둥에서 보조 기둥으로 이동하도록 함수를 다시 부름
         hanoi(n - 1, start, via, destination)
-        count += 1
-    # 가장 큰 원반을 시작 기둥에서 도착 기둥으로 이동
-    
+        
+    # 가장 큰 원반을 시작 기둥에서 도착 기둥으로 실질적인 이동
         print(MOVE_MESSAGE.format(n, start, destination))
+        count += 1  # 실질적인 이동 카운트
     
-    # 원반 n-1개를 보조 기둥에서 도착 기둥으로 이동
+    # 원반 n-1개를 보조 기둥에서 도착 기둥으로 이동하도록 함수를 다시부름
         hanoi(n - 1, via, destination, start)
       
-        # print(MOVE_MESSAGE.format(n, start, destination))    
-   
 
 if __name__ == '__main__':
     
